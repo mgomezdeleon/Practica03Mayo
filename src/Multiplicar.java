51ae606e9997fac1112434affb19fd6deeabbd87
CallejoKiller <acallejob@ceinmark.net>
2023-05-03 17:50:11 +0200

    Ejercicio Andres Callejo

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -1,19 +1,29 @@
 public class Multiplicar {
 
 	public static void main(String[] args) {
-		
-		// Práctica Tablas de Multiplicar
-			saludar();
-			
-		//*****************************************
-		// Crear una función que imprima las 10 tablas de 
+
+		// Prï¿½ctica Tablas de Multiplicar
+		saludar();
+
+		// *****************************************
+		// Crear una funciï¿½n que imprima las 10 tablas de
 		// multiplicar
-		//*****************************************
-		
-		
+		// *****************************************
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("Tabla de multiplicar del " + i + ":");
+			System.out.println("-----------------------------");
+
+			for (int j = 0; j <= 10; j++) {
+				System.out.println(i + " x " + j + " = " + (i * j));
+			}
+
+			System.out.println();
+		}
 	}
+
 	public static void saludar() {
-		
-		System.out.println("Buenos tardes 1ºde Dam!!!");
+
+		System.out.println("Buenos tardes 1ï¿½de Dam!!!");
 	}
 }

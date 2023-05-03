523b6b6d92d42ef630ecc1d65658fbbff3b45f30
Marta <mgomezdeleon@ceinmark.net>
2023-05-03 15:52:30 +0200

    Primer commit

---
+++ src/Multiplicar.java
@@ -0,0 +1,19 @@
+public class Multiplicar {
+
+	public static void main(String[] args) {
+		
+		// Práctica Tablas de Multiplicar
+			saludar();
+			
+		//*****************************************
+		// Crear una función que imprima las 10 tablas de 
+		// multiplicar
+		//*****************************************
+		
+		
+	}
+	public static void saludar() {
+		
+		System.out.println("Buenos tardes 1ºde Dam!!!");
+	}
+}

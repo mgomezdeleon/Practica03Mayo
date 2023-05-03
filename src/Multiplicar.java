a47ddb8bb7e7615d5c4cdb0818a604ee1edbbfb7
vgm-ceinmark <vgarciam@ceinmark.net>
2023-05-03 17:30:30 +0200

    10 tablas de multiplicar

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -2,18 +2,28 @@ public class Multiplicar {
 
 	public static void main(String[] args) {
 		
-		// Práctica Tablas de Multiplicar
+		// Prï¿½ctica Tablas de Multiplicar
 			saludar();
 			
 		//*****************************************
-		// Crear una función que imprima las 10 tablas de 
+		// Crear una funciï¿½n que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
+		tablasMultiplicar();
 		
 		
 	}
 	public static void saludar() {
-		
-		System.out.println("Buenos tardes 1ºde Dam!!!");
+		System.out.println("Buenos tardes 1ï¿½de Dam!!!");
 	}
+	
+	private static void tablasMultiplicar() {
+		for(int i=1;i<11;i++) {
+			System.out.println("\nTabla del "+i+":");
+			for(int j=0;j<11;j++) {
+				System.out.println(i+" * "+j+" = "+(i*j));
+			}
+		}
+	}
+	
 }

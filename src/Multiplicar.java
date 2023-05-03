3f38e56913e9c80241a62f713db6276fa86d60b0
Usuario <Usuario@LAPTOP-HQ6AESQV>
2023-05-03 18:28:54 +0200

    Modificado por Germán

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -16,4 +16,14 @@ public class Multiplicar {
 		
 		System.out.println("Buenos tardes 1ºde Dam!!!");
 	}
+	
+	public static void imprimirTablasDeMultiplicar() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabla del " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i*j));
+            }
+            System.out.println();
+        }
+	}
 }

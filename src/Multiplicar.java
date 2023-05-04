6b4166abf617db159cf9c71f19947e33a7184d07
irene <irene@Billy>
2023-05-04 18:14:18 +0200

    Subida de Irene de las tablas.

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -2,18 +2,30 @@ public class Multiplicar {
 
 	public static void main(String[] args) {
 		
-		// Práctica Tablas de Multiplicar
+		// Prï¿½ctica Tablas de Multiplicar
 			saludar();
 			
 		//*****************************************
-		// Crear una función que imprima las 10 tablas de 
+		// Crear una funciï¿½n que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
+			
+			tablasDeMultiplicar();
 		
 	}
 	public static void saludar() {
 		
-		System.out.println("Buenos tardes 1ºde Dam!!!");
+		System.out.println("Buenos tardes 1ï¿½de Dam!!!");
+	}
+	
+	public static void tablasDeMultiplicar() {
+	    for (int i = 1; i <= 10; i++) {
+	        System.out.println("Tabla del " + i + ":");
+	        for (int j = 1; j <= 10; j++) {
+	            int resultado = i * j;
+	            System.out.println(i + " x " + j + " = " + resultado);
+	        }
+	        System.out.println(); // Salto de lÃ­nea para separar las tablas
+	    }
 	}
 }

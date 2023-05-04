3303a03a2e14b29dfa4b5b334e58a7d7422eee2b
mtirador <125604629+mtirador@users.noreply.github.com>
2023-05-04 20:19:22 +0200

    Modificado por Marta Tirador

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -10,10 +10,23 @@ public class Multiplicar {
 		// multiplicar
 		//*****************************************
 		
+			String linea="";
+			for(int i=1; i<=10; i++) {
+				for(int j=1;j<=10;j++) {
+					linea+= j + " * "+ i + "= "+(j*i)+ "\t";
+					
+				}
+				System.out.println(linea);
+				linea="";
+			}
+			System.out.println("modificado por Marta Tirador");
+			
 		
 	}
 	public static void saludar() {
 		
 		System.out.println("Buenos tardes 1ºde Dam!!!");
 	}
+	
+	
 }

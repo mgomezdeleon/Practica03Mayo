b4c868d21d30e860dc0660a1f00cb0fa0898c752
Daniel Barba <Daniel Barba@DESKTOP-56VKP46>
2023-05-04 11:11:09 +0200

    Ejercicio Daniel Barba

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -15,5 +15,12 @@ public class Multiplicar {
 	public static void saludar() {
 		
 		System.out.println("Buenos tardes 1ºde Dam!!!");
+		
+		for (int i = 1; i <= 10 ; i ++) {
+			System.out.println("Tabla del " + i + ":");
+			for (int j = 1 ; j <= 10 ; j++) {
+				System.out.println(i + " * " + j + " = " + (i*j) );
+			}
+		}
 	}
 }

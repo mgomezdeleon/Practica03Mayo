034dadce70c421480a82a92e2b6791a0f24d4b57
lastr <lastr@BelialPortable>
2023-05-05 12:33:06 +0200

    Sergio

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -9,11 +9,21 @@ public class Multiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
+		tablasMultiplicar();
 		
 	}
 	public static void saludar() {
 		
 		System.out.println("Buenos tardes 1ºde Dam!!!");
+	}
+	
+	public static void tablasMultiplicar() {
+		
+		for(int i=1;i<11;i++) {
+			System.out.println("\nTabla de Multiplicar del "+i+":\n");
+			for(int j=0;j<11;j++) {
+				System.out.println("\t"+i+" * "+j+" = "+(i*j));
+			}
+		}
 	}
 }

56078a6841d7982fadcd791af86f5db71ebd7f61
Usuario <Usuario@DESKTOP-EBETHVO>
2023-05-05 12:33:48 +0200

    Alvaro

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -2,18 +2,27 @@ public class Multiplicar {
 
 	public static void main(String[] args) {
 		
-		// Práctica Tablas de Multiplicar
+		// Practica Tablas de Multiplicar
 			saludar();
 			
 		//*****************************************
-		// Crear una función que imprima las 10 tablas de 
+		// Crear una funcion que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
-		
+			
+		for (int i = 1; i < 11; i++) {
+			System.out.println("--> Tabla del " + i + " <--");
+			for (int j = 0; j <= 10; j++) {
+				System.out.println(i + " x " + j + " = " + (i * j));
+			}
+		System.out.println("\n");
+			
+		}
 	}
+	
 	public static void saludar() {
-		
-		System.out.println("Buenos tardes 1ºde Dam!!!");
+		System.out.println("Buenos tardes 1Âº de Dam!!!\n");
 	}
 }
+
+

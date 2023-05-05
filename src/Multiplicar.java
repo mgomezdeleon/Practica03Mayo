b88ce39da3089e223c15b4bd9ae9c962d7f03e09
Pablo <Pablo@DESKTOP-FVUDCBN>
2023-05-05 12:45:26 +0200

    Pablo

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -9,11 +9,21 @@ public class Multiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
+			tablasMultiplicar();
 		
 	}
 	public static void saludar() {
 		
 		System.out.println("Buenos tardes 1ºde Dam!!!");
+		
+	}
+	
+	private static void tablasMultiplicar() {
+		for(int i=1;i<11;i++) {
+			System.out.println("\nTabla del "+i+":");
+			for(int j=0;j<11;j++) {
+				System.out.println(i+" * "+j+" = "+(i*j));
+			}
+		}
 	}
 }

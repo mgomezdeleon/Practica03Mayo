43acba050099952af9572eaa34f5148cf1531969
Javier <Javier@DESKTOP-7O8P9C8>
2023-05-07 16:48:12 +0200

    tablas de multiplicar (Javi)

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -2,18 +2,25 @@ public class Multiplicar {
 
 	public static void main(String[] args) {
 		
-		// Práctica Tablas de Multiplicar
+		// Prï¿½ctica Tablas de Multiplicar
 			saludar();
 			
 		//*****************************************
-		// Crear una función que imprima las 10 tablas de 
+		// Crear una funciï¿½n que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
+			
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=10; j++) {
+				int r=i*j;
+				System.out.println(i+" x "+j+" = "+r);
+			}
+		}
 		
 		
 	}
 	public static void saludar() {
 		
-		System.out.println("Buenos tardes 1ºde Dam!!!");
+		System.out.println("Buenos tardes 1ï¿½de Dam!!!");
 	}
 }

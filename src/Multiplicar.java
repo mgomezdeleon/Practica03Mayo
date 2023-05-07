79ac56fd8a52f40a534f517e40fbbc46faedf135
win10 <win10@DESKTOP-QQMM0I2>
2023-05-07 20:40:45 +0200

    commit

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -2,18 +2,28 @@ public class Multiplicar {
 
 	public static void main(String[] args) {
 		
-		// Práctica Tablas de Multiplicar
+		// Prï¿½ctica Tablas de Multiplicar
 			saludar();
 			
 		//*****************************************
-		// Crear una función que imprima las 10 tablas de 
+		// Crear una funciï¿½n que imprima las 10 tablas de 
 		// multiplicar
+			multiplicar();
 		//*****************************************
 		
 		
 	}
 	public static void saludar() {
 		
-		System.out.println("Buenos tardes 1ºde Dam!!!");
+		System.out.println("Buenos tardes 1ï¿½de Dam!!!");
+	}
+	
+	public static void multiplicar() {
+		for(int i=0; i<=10; i++) {
+			System.out.println(" \n" + "Tabla del " + i);
+			for(int j=0; j<=10; j++) {
+				System.out.println(i + "*" + j + "=" + i*j);
+			}
+		}
 	}
 }

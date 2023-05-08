64935b79b215b50734bf529b768b53a70e7dde5c
ALUMNOS <ALUMNOS@P618>
2023-05-08 09:13:02 +0200

    yoel

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -9,7 +9,16 @@ public class Multiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
+			printMultiplicationTables();
+	}
+	public static void printMultiplicationTables() {
+		for (int i = 1; i <= 10; i++) {
+        System.out.println("Tabla del " + i + ":");
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(i + " x " + j + " = " + (i*j));
+        }
+        System.out.println();
+		}
 		
 	}
 	public static void saludar() {
